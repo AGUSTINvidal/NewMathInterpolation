94916da16d00a22be30f6d24ab0636619bef6caa
Agustin Vidal <tin_tilin@hotmail.com>
2018-09-16 03:14:22 -0700

    Creacion de clases Test para pruebas

--- ALL/src/UNIPruebas/CubicSplineTest.java
+++ ALL/src/UNIPruebas/CubicSplineTest.java
@@ -1,4 +1,29 @@
 package UNIPruebas;
 
+import com.demo.interpol.CubicSpline;
+import org.junit.Test;
+import java.util.Vector;
+import static org.junit.Assert.assertEquals;
+
 public class CubicSplineTest {
+    @Test
+    public void calculateResultTest(){
+        double elresulEsperado=1.600000023841858;
+        Vector<Double> pri = new Vector<Double>();
+        double obtenido=0.0;
+        Vector<Double> seg = new Vector<>();
+        double tolerancia=0.0;
+        pri.addElement(2.3);
+        pri.addElement(3.3);
+        pri.addElement(4.3);
+        pri.addElement(5.3);
+        pri.addElement(6.3);
+        seg.addElement(1.5);
+        seg.addElement(2.5);
+        seg.addElement(3.5);
+        seg.addElement(4.5);
+        seg.addElement(5.5);
+        obtenido= new CubicSpline().calculateResult(2.4, pri, seg);
+        assertEquals(elresulEsperado,obtenido,tolerancia);
+    }
 }
--- ALL/src/UNIPruebas/LagrangeTest.java
+++ ALL/src/UNIPruebas/LagrangeTest.java
@@ -1,4 +1,30 @@
 package UNIPruebas;
 
-public class LagrageTest {
+import com.demo.interpol.Lagrange;
+import org.junit.Test;
+import java.util.Vector;
+import static org.junit.Assert.assertEquals;
+
+public class LagrangeTest
+{
+    @Test
+    public void LagrangeResultTest(){
+        double elresulEsperado=1.6000000000000003;
+        Vector<Double> pri = new Vector<Double>();
+        double obtenido=0.0;
+        Vector<Double> seg = new Vector<>();
+        double tolerancia=0.0;
+        pri.addElement(2.3);
+        pri.addElement(3.3);
+        pri.addElement(4.3);
+        pri.addElement(5.3);
+        pri.addElement(6.3);
+        seg.addElement(1.5);
+        seg.addElement(2.5);
+        seg.addElement(3.5);
+        seg.addElement(4.5);
+        seg.addElement(5.5);
+        obtenido= new Lagrange().calculateResult(2.4, pri, seg);
+        assertEquals(elresulEsperado,obtenido,tolerancia);
+    }
 }
